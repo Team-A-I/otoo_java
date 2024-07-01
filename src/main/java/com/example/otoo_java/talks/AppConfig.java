faf35cdce3f75f744b3cd77c5dcf75b6c0abe919
khe8895 <khe8895@gachon.ac.kr>
2024-07-01 16:13:40 +0900

    first_commit

--- src/main/java/com/example/otoo_java/talks/AppConfig.java
+++ src/main/java/com/example/otoo_java/talks/AppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
-
+//확인
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
