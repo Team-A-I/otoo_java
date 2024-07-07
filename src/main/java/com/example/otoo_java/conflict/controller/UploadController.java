2ec53739f799ab8f1053e74e28123b586484a1c3
배정현\bjh73 <bae4445@gmail.com>
2024-07-07 23:49:11 +0900

    [Feature] fastAPI 엔드포인트 변경

--- src/main/java/com/example/otoo_java/conflict/controller/UploadController.java
+++ src/main/java/com/example/otoo_java/conflict/controller/UploadController.java
@@ -14,7 +14,7 @@ public class UploadController {
     @Value("${fastapi.url}")
     private String fastApiUrl;
 
-    @PostMapping("/upload")
+    @PostMapping("/analysis")
     public ResponseEntity<String> handleFileUpload(@RequestBody JsonNode jsonNode) {
         try {
             RestTemplate restTemplate = new RestTemplate();
@@ -29,7 +29,6 @@ public class UploadController {
 
             return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
         } catch (HttpClientErrorException e) {
-            // FastAPI 서버로부터 받은 오류 메시지를 출력합니다.
             return new ResponseEntity<>(e.getResponseBodyAsString(), e.getStatusCode());
         } catch (Exception e) {
             e.printStackTrace();
