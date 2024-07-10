8067cebc3b6efe1dc831c287e28b5298a8e92481
baejeonghyun <159854114+baejeonghyun23@users.noreply.github.com>
2024-07-10 14:46:50 +0900

    Update OtooJavaApplicationTests.java

--- src/test/java/com/project/otoo_java/OtooJavaApplicationTests.java
+++ src/test/java/com/project/otoo_java/OtooJavaApplicationTests.java
@@ -3,7 +3,7 @@ package com.project.otoo_java;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = TakeItEasyApplicationTests.class)
+@SpringBootTest(classes = OtooJavaApplicationTests.class)
 class OtooJavaApplicationTests {
 
     @Test
