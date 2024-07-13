e9dab4778f69a200259ca803d3f1258e9901b692
khe8895 <khe8895@gachon.ac.kr>
2024-07-13 18:29:52 +0900

    [Feature]1차배포용 API수정

--- src/main/java/com/project/otoo_java/analyze/controller/AnalyzeController.java
+++ src/main/java/com/project/otoo_java/analyze/controller/AnalyzeController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.otoo_java.analyze.dto.AnalyzeDto;
 import com.project.otoo_java.analyze.service.AnalyzeService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -27,7 +28,8 @@ public class AnalyzeController {
 
     private final AnalyzeService analyzeService;
 
-    private static final String FASTAPI_URL = "http://localhost:8001/analyze";
+    @Value("{fastapi.url}")
+    private String FASTAPI_URL;
     private static final Logger logger = LoggerFactory.getLogger(AnalyzeController.class);
 
     public AnalyzeController(AnalyzeService analyzeService) { // 생성자 추가
@@ -68,7 +70,7 @@ public class AnalyzeController {
             }
 
             ResponseEntity<String> response = restTemplate.exchange(
-                    FASTAPI_URL,
+                    FASTAPI_URL + "/analyze",
                     HttpMethod.POST,
                     request,
                     String.class
