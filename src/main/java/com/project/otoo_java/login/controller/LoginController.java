1959eae54fdf1997236628cd25151dd9116e206b
배정현 <bae4445@gmail.com>
2024-07-24 13:08:01 +0900

    [Feature] 로그아웃시 세션 초기화

--- src/main/java/com/project/otoo_java/login/controller/LoginController.java
+++ src/main/java/com/project/otoo_java/login/controller/LoginController.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -93,6 +94,14 @@ public class LoginController {
         String email = request.getHeader("Authorization");
         log.info("email: " + email);
         redisTemplate.delete("JWT_TOKEN:" + email);
+
+        // 세션 무효화
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
 }
